c7256d848fbb29d13965a274caaa4bc7aaee0bc6
Emiljano <Emiljanokurbiba@gmail.com>
2022-07-03 16:35:12 +0200

    First Submission
    Simple draft application using Python Flask & Bootstrap rerouting coming soon along with requirements document. Documentation will come soon after I have a more advanced draft.

---
+++ app.py
@@ -0,0 +1,9 @@
+from ensurepip import bootstrap
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
