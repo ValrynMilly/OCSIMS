92ef35f9157c356351df0d809f3f70c12faec529
Emiljano <Emiljanokurbiba@gmail.com>
2022-07-13 19:19:32 +0200

    Laptops table added to db and showing in pages
    Decided that the tool should only have 1 database with multiple tables. I have added a laptops table which actually shows up on the page. You must however always be logged in to view these pages, I will add a form that users can add and delete from the table so its easier to manage. Simple table showcase successful.

--- IMSAPP/project/main.py
+++ IMSAPP/project/main.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template
 from flask_login import login_required, current_user
 from . import db
+from .models import Laptops
 
 main = Blueprint('main', __name__)
 
@@ -12,3 +13,9 @@ def index():
 @login_required
 def profile():
     return render_template('profile.html', name=current_user.name)
+
+@main.route('/laptops')
+@login_required
+def laptops():
+    laptop = Laptops.query
+    return render_template('laptops.html', laptop=laptop)
--- IMSAPP/project/models.py
+++ IMSAPP/project/models.py
@@ -1,3 +1,4 @@
+from enum import unique
 from flask_login import UserMixin
 from . import db
 
@@ -6,3 +7,13 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
     name = db.Column(db.String(1000))
+
+class Laptops(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    manufactor = db.Column(db.String(50), unique=True)
+    model = db.Column(db.String(50))
+    cpu = db.Column(db.String(50))
+    ram = db.Column(db.String(50))
+    storage = db.Column(db.String(50))
+    operating_system = db.Column(db.String(50))
+    assigned = db.Column(db.Boolean)
